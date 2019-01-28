b8d0eb56000589f79fa6d148cb8beade604b195b
alonso0492 <carlos0492@outlook.com>
2019-01-27 21:25:07 -0500

    Rename files

--- Disable-WindowsLoginAnimaiton/DisableWindows-LoginAnimation.bat
+++
@@ -1,7 +0,0 @@
-:: Carlos Martinez Github @cmartinezone   Date:1/27/2018
-:: Windows 10 customization (  Disable Windows Login Animation)
-
-
-:: Disable Windows Login Animation
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "EnableFirstLogonAnimation" /t REG_DWORD /d "0" /f
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v "EnableFirstLogonAnimation" /t REG_DWORD /d "0" /f
