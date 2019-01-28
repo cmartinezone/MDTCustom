59a365f5125708201d3fa8b69b5ba7629ddae605
alonso0492 <carlos0492@outlook.com>
2019-01-27 21:35:23 -0500

    Remove Windows Apps provissioned

---
+++ Remove-AppsProvissioned/Remove-AppsProvissioned.ps1
@@ -0,0 +1,55 @@
+﻿# Carlos Martinez 8/13/18 GitHub @cmartinezone
+
+#Add the Provisioned Windows apps that you or your organization don't want for a new user
+#Apps Reference: https://docs.microsoft.com/en-us/windows/application-management/apps-in-windows-10
+
+$WhiteListedApps = @(
+"Microsoft.3DBuilder"
+"Microsoft.BingWeather"
+"microsoft.windowscommunicationsapps"
+"Microsoft.DesktopAppInstaller"
+"Microsoft.HEIFImageExtension"
+"Microsoft.Microsoft3DViewer"
+"Microsoft.MicrosoftStickyNotes"
+"Microsoft.MixedReality.Portal"
+"Microsoft.MSPaint"
+"Microsoft.Print3D"
+"Microsoft.ScreenSketch"
+"Microsoft.StorePurchaseApp"
+"Microsoft.VP9VideoExtensions"
+"Microsoft.Wallet"
+"Microsoft.WebMediaExtensions"
+"Microsoft.HEVCVideoExtension"
+"Microsoft.WebpImageExtension"
+"Microsoft.Windows.Photos"
+"Microsoft.WindowsAlarms"
+"Microsoft.WindowsCalculator"
+"Microsoft.WindowsCamera"
+"Microsoft.WindowsMaps"
+"Microsoft.WindowsSoundRecorder"
+"Microsoft.WindowsStore"
+"Microsoft.YourPhone"
+"Microsoft.ZuneMusic"
+"Microsoft.ZuneVideo"
+)
+
+# To use this script with offline image state:  Get-AppxProvisionedPackage -Path "c:\offline"
+
+try { $apps = Get-AppxProvisionedPackage -online | ForEach-Object {
+
+    if (($_.DisplayName -notin $WhiteListedApps)) {
+    Write-Host "Removing the following Built-in app:" -ForegroundColor Green
+    Write-Host "Deprovisioned:" $_.DisplayName -ForegroundColor yellow
+    Remove-AppxProvisionedPackage -online -PackageName $_.PackageName } 
+    }
+    
+}
+	
+catch [Exception] {
+    Write-Host "Removing Built-in apps failed..." -ForegroundColor Red;
+    Write-Host "Error:" $_.Exception.Message -ForegroundColor Red; 
+    
+}
+
+
+   
