6e8693f4f5b3bd4ef2d4afc20fb8e688e14b91e7
Carlos Martinez <carlos0492@outlook.com>
2019-03-05 12:07:46 -0500

    Restore Packages
    Restore windows apps packages under regular user

---
+++ Restore-WindowsApps/User-Level/RestoreAppPackages.ps1
@@ -0,0 +1,13 @@
+﻿# Developed by  Carlos Martinez
+#Add-packages on User level from the text file generated with EnablePhotoViwerGetPackages.ps1
+
+#Standard  user
+$path = "C:\temp\AppsPackages.txt"
+if ( test-path $path ){ 
+Get-Content C:\temp\AppsPackages.txt | foreach { Add-AppxPackage -DisableDevelopmentMode -Register $_ } 
+remove-item $path
+}
+else { Write-Host "No File found, please run EnablePhotoViwerGetPackages.ps1 as administrator First!"  
+sleep 5 
+exit
+ }
