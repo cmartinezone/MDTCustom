48dfea148f5a96e0fc04278b340f2fd94ce09868
cmartinezone <carlos0492@outlook.com>
2019-03-24 01:08:50 -0400

    Office updater
    Microsoft Office updater

---
+++ MSOffice-Updater-v1.0/OfficeUpdaterV1.0.ps1
@@ -0,0 +1,32 @@
+﻿
+#Carlos Martinez Github @cmartinezone
+
+#Microsoft Office updater v.10 - 3-24-2019
+#This script update Microsoft Office already installed on the system
+#It will stay running if the update starts for 5 minutes. It can be integrated using MDT as a single task.
+
+$OfficeLocation = "C:\Program Files\Common Files\Microsoft Shared\ClickToRun\OfficeC2RClient.exe"
+$Arguments = "/update user updatepromptuser=False forceappshutdown=True displaylevel=False"
+
+#Testing chaing channel 
+#$MonthlyChannel = "/changesetting Channel=FirstReleaseCurrent"
+#Start-Process -FilePath $OfficeLocation -ArgumentList $MonthlyChannel -wait -NoNewWindow 
+
+Start-Process -FilePath $OfficeLocation -ArgumentList $Arguments -wait -NoNewWindow 
+
+
+$OfficeUpdateProcess = Get-Process -Name "OfficeClickToRun"
+Start-Sleep -Seconds 10
+
+
+if ($OfficeUpdateProcess.Count -ge 2 ) {  
+    
+    Write-Progress -Activity 'MicrosoftOffice:' -Status 'Updating...' -PercentComplete 100 
+    Start-Sleep -Seconds 240
+    Stop-Process -Name  "OfficeClickToRun" -Force
+
+}
+
+
+
+
