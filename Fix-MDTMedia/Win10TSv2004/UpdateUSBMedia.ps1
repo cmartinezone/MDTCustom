d47fabe262f7fc5226aa0b23d00694e5265ce208
Carlos Martinez <carlos0492@outlook.com>
2020-10-14 00:44:58 -0400

    update

--- Fix-MDTMedia/Win10TSv2004/UpdateUSBMedia.ps1
+++ Fix-MDTMedia/Win10TSv2004/UpdateUSBMedia.ps1
@@ -6,15 +6,14 @@
 #Detect USB Task Sequence Media
 Get-Volume | Select-Object -Property DriveLetter -ExpandProperty DriveLetter | % { 
         
-    If( Test-Path -Path  $_":\Deploy\Control\WIN10V2004OFF19\ts.xml"){
+    If ( Test-Path -Path  $_":\Deploy\Control\WIN10V2004OFF19\ts.xml") {
       
-        #Download Ts Updated
-        
 
-        #Download patch zip 
-
-        Write-host "Found USB Media"
-
+        # Download the file to a specific location
+        $clnt = new-object System.Net.WebClient
+        $url = "https://git.io/JTm0H"
+        $file = "E:\patch.zip"
+        $clnt.DownloadFile($url, $file)
     }
 
 }
